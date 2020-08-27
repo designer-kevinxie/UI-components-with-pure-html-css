d95c5338c3d0affdd585ddaf1f39b4e61eac94a6
designer-kevinxie <designer.kevinxie@gmail.com>
2020-08-27 12:19:27 +0800

    update content card 01:add animation with js

--- content-card-01/index.js
+++ content-card-01/index.js
@@ -1,12 +1,13 @@
 var cards = document.querySelectorAll(".card-item");
 
 for(var i = 0 ;i<cards.length;i++){
+  console.log(cards[i]);
 
-   document.querySelectorAll(".card-item")[i].addEventListener("mouseenter",function(){
+   cards[i].addEventListener("mouseenter",function(){
   this.classList.toggle("card-hover");
 });
 
-document.querySelectorAll(".card-item")[i].addEventListener("mouseleave",function(){
+cards[i].addEventListener("mouseleave",function(){
 this.classList.toggle("card-hover");
 });
 
